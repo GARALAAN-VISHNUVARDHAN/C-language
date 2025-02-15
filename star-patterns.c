76d59ba3281daf8ae31c405bfcc484c49a3c0998
GARALAAN-VISHNUVARDHAN <vvishnuvardhan58@gmail.com>
2025-02-15 21:55:51 +0530

    Update star-patterns.c

--- star-patterns.c
+++ star-patterns.c
@@ -56,4 +56,30 @@ int main(){
 // 	*	*	*	*
 	
 // 	*	*	*	*	*
-	
+#include<stdio.h>
+int main(){
+    int i,j,rows;
+    printf("enter number of rows");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++){
+      for(j=1;j<=rows-i;j++)
+      {
+          printf("\t");
+      }
+          // Print stars
+    for(j = 1; j <= (2 * i - 1); j++) {
+        printf("*\t");
+    }
+    
+    // Move to the next line
+    printf("\n");
+
+      }
+    }
+//     output:
+//     enter number of rows5
+// 				*	
+// 			*	*	*	
+// 		*	*	*	*	*	
+// 	*	*	*	*	*	*	*	
+// *	*	*	*	*	*	*	*	*	
