3cd86342caabaf36e85ed0446e9736adf23af058
GARALAAN-VISHNUVARDHAN <vvishnuvardhan58@gmail.com>
2025-02-15 21:36:59 +0530

    Create simple-Do-While.c

---
+++ simple-Do-While.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n=10, i=5;
+    do
+    {
+        printf("hello %d\n",i);
+        i++;
+    }
+    while(i<=n);
+    
+}
+//output
+// hello 5
+// hello 6
+// hello 7
+// hello 8
+// hello 9
+// hello 10
+
