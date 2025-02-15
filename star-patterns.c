9c4478a783be46747ae04006b8575a52d102a14c
GARALAAN-VISHNUVARDHAN <vvishnuvardhan58@gmail.com>
2025-02-15 21:50:08 +0530

    Create star-patterns.c

---
+++ star-patterns.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+    int i,j,rows;
+    printf("enter number of rows");
+    scanf("%d",&rows);
+    for(i=rows;i>=1;i--){
+    for(j=1;j<=i;j++){
+       
+        printf("\t*");
+        
+        
+    }
+     printf("\n\t");
+    printf("\n");
+        
+    }
+    
+}
+// output:
+// enter number of rows5
+// 	*	*	*	*	*
+	
+// 	*	*	*	*
+	
+// 	*	*	*
+	
+// 	*	*
+	
+// 	*
+#include<stdio.h>
+int main(){
+    int i,j,rows;
+    printf("enter number of rows");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++){
+    for(j=1;j<=i;j++){
+       
+        printf("\t*");
+        
+        
+    }
+     printf("\n\t");
+    printf("\n");
+        
+    }
+    
+}
+// output:
+// enter number of rows5
+// 	*
+	
+// 	*	*
+	
+// 	*	*	*
+	
+// 	*	*	*	*
+	
+// 	*	*	*	*	*
+	
