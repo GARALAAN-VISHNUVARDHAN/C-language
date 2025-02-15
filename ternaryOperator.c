b7cd44952d88faa300665d30af1b10b9fe305189
GARALAAN-VISHNUVARDHAN <vvishnuvardhan58@gmail.com>
2025-02-15 21:33:38 +0530

    Update ternaryOperator.c

--- ternaryOperator.c
+++ ternaryOperator.c
@@ -3,3 +3,14 @@ int main() {
     int a = 10, b = 20;
     (a > b) ? printf("a is greater") : printf("b is greater ");
 }
+
+
+#include<stdio.h>
+int main()
+{
+    int a =10,b=20, c=20;
+    b=(a==1) ?20:30;
+    printf(" b values is %d:\n",b);
+    c=(c==20) ?30:3;
+    printf(" c values is %d:\n",c);
+}   
